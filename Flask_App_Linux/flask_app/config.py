9732037a58e61f77822d2a279d14400e3717b1bd
swaroopgorur <swaroopgorur@gmail.com>
2024-09-10 12:50:21 +0530

    Adding changes in Linux Files

--- Flask_App_Linux/flask_app/config.py
+++ Flask_App_Linux/flask_app/config.py
@@ -1,17 +1,13 @@
 import os
-import json
-
-with open('/etc/flask_blog_config.json') as config_file:
-    config = json.load(config_file)
-
 
 class Config:
     #created using the secrets module
     #secrets.token_hex(16) ---> 'b4b1b3011043cf50e962c2c2dab8ac2c'
-    SECRET_KEY = config.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config.get('MAIL_USER')
-    MAIL_PASSWORD = config.get('MAIL_PASS')
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
+    PERSPECTIVE_API_KEY = os.environ.get('PERSPECTIVE_API_KEY')
